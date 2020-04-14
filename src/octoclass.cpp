f6e4f8aefb240fbcad4fc04cd9e61c2a6b6d8251
Marcelino <marcelino.malmeidan.pensa@gmail.com>
2020-04-14 18:30:59 -0500

    commit for debug tests to be removed later

--- src/octoclass.cpp
+++ src/octoclass.cpp
@@ -346,6 +346,7 @@ void OctoClass::PclToRayOctomap(const pcl::PointCloud< pcl::PointXYZ > &cloud,
     static octomap::point3d central_point, cur_point;
     const double max_range_sqr = max_range_*max_range_;
     pcl::PointXYZ point;
+    int nan_count = 0, zero_count = 0;;
     for (uint32_t i = 0; i < height; i++) {
         for (uint32_t j = 0; j < width; j++) {
             if (cloud.is_dense) {
@@ -360,6 +361,7 @@ void OctoClass::PclToRayOctomap(const pcl::PointCloud< pcl::PointXYZ > &cloud,
 
             // Check if the point is invalid
             if (std::isnan(point.x) || std::isnan(point.y) || std::isnan(point.z)) {
+                nan_count++;
                 continue;
             }
 
@@ -373,6 +375,10 @@ void OctoClass::PclToRayOctomap(const pcl::PointCloud< pcl::PointXYZ > &cloud,
             // }
             if ((range_sqr < min_threshold_sqr)) {
                 continue;
+            }
+
+            if (range_sqr == 0) {
+                zero_count++;
             }
 
             // create discretized octocloud
@@ -396,6 +402,8 @@ void OctoClass::PclToRayOctomap(const pcl::PointCloud< pcl::PointXYZ > &cloud,
             }
         }
     }
+    std::cout << "nan_count: "  << nan_count  << std::endl;
+    std::cout << "zero_count: " << zero_count << std::endl;
     // std::cout << "Closest range: " << sqrt(closest_range_sqr) << std::endl;
     // std::cout << "Closest point: " << closest_point.transpose() << std::endl;
     // std::cout << "Current point: " << cur_position.transpose() << std::endl;
